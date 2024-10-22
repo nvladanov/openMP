ce17d584cc9b834e4657fdb9ee3b9112f986b266
Ladanov_Nick <nv.ladanov@gmail.com>
2024-10-22 15:44:54 +0200

    feat: simple implementation gpsa_taskloop

--- implementation.hpp
+++ implementation.hpp
@@ -35,28 +35,38 @@ unsigned long SequenceInfo::gpsa_taskloop(float** S, int grain_size=1) {
 	// your work goes here, this is just a sequential code
 	unsigned long visited = 0;
 
-	// Boundary
+    // Initialize the first row and column
+    #pragma omp parallel for
     for (unsigned int i = 1; i < rows; i++) {
         S[i][0] = i * gap_penalty;
-		visited++;
-	}
+        visited++;
+    }
 
+    #pragma omp parallel for
     for (unsigned int j = 0; j < cols; j++) {
         S[0][j] = j * gap_penalty;
-		visited++;
-	}
+        visited++;
+    }
 	
-	// Main part
-	for (unsigned int i = 1; i < rows; i++) {
-		for (unsigned int j = 1; j < cols; j++) {
-			float match = S[i - 1][j - 1] + (X[i - 1] == Y[j - 1] ? match_score : mismatch_score);
-			float del = S[i - 1][j] + gap_penalty;
-			float insert = S[i][j - 1] + gap_penalty;
-			S[i][j] = std::max({match, del, insert});
-		
-			visited++;
-		}
-	}
+    // Compute the similarity matrix using taskloop
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            for (unsigned int i = 1; i < rows; i++) {
+                #pragma omp taskloop grainsize(grain_size)
+                for (unsigned int j = 1; j < cols; j++) {
+                    float match = S[i-1][j-1] + (X[i-1] == Y[j-1] ? match_score : mismatch_score);
+                    float del = S[i-1][j] + gap_penalty;
+                    float insert = S[i][j-1] + gap_penalty;
+                    S[i][j] = std::max({match, del, insert});
+
+                    #pragma omp atomic
+                    visited++;
+                }
+            }
+        }
+    }
 
     return visited;
 }
