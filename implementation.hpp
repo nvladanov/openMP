6579cabb91854ca71806058f453171889c4e2671
Ladanov_Nick <nv.ladanov@gmail.com>
2024-10-23 15:06:06 +0200

    feat: first attempt

--- implementation.hpp
+++ implementation.hpp
@@ -37,15 +37,21 @@ unsigned long SequenceInfo::gpsa_taskloop(float** S, int grain_size = 1) {
     int gap_penalty = -2;
     int match_score = 1, mismatch_score = -1;
 
-    for (unsigned int i = 1; i < rows; i++) {
-        S[i][0] = i * gap_penalty;
-		visited++;
-	}
+    #pragma omp parallel
+    #pragma omp single
+    {
+        #pragma omp taskloop grainsize(512) reduction(+:visited)
+        for (unsigned int i = 1; i < rows; i++) {
+            S[i][0] = i * gap_penalty;
+            visited++;
+        }
 
-    for (unsigned int j = 0; j < cols; j++) {
-        S[0][j] = j * gap_penalty;
-		visited++;
-	}
+        #pragma omp taskloop grainsize(512) reduction(+:visited)
+        for (unsigned int j = 0; j < cols; j++) {
+            S[0][j] = j * gap_penalty;
+            visited++;
+        }
+    }
 
     unsigned int total_diagonals = rows + cols - 1;
 
@@ -54,8 +60,8 @@ unsigned long SequenceInfo::gpsa_taskloop(float** S, int grain_size = 1) {
         #pragma omp single
         {
             for (unsigned int k = 2; k <= total_diagonals - 1; k++) {
-                unsigned int i_start = (std::max)(1u, k >= cols ? k - cols + 1u : 1u);
-                unsigned int i_end = (std::min)(rows - 1u, k - 1u);
+                unsigned int i_start = (k <= cols - 1) ? 1 : k - (cols - 1);
+                unsigned int i_end   = (k <= rows - 1) ? k - 1 : rows - 1;
 
                 #pragma omp taskloop grainsize(grain_size) reduction(+:visited)
                 for (unsigned int i = i_start; i <= i_end; i++) {
@@ -68,7 +74,7 @@ unsigned long SequenceInfo::gpsa_taskloop(float** S, int grain_size = 1) {
                         visited++;
                     }
                 }
-                #pragma omp taskwait // Ensure all tasks in the current anti-diagonal are completed
+                #pragma omp taskwait
             }
         }
     }
