9065a9763477af0c069b845a2c4ec3afbb260f81
Ladanov_Nick <nv.ladanov@gmail.com>
2024-10-23 11:29:45 +0200

    feat: first attempt

--- implementation.hpp
+++ implementation.hpp
@@ -41,19 +41,21 @@ unsigned long SequenceInfo::gpsa_taskloop(float** S, int block_size = 1) {
         #pragma omp single
         {
             // Create tasks for both the first column and first row in parallel
-            #pragma omp taskloop grainsize(64) reduction(+:visited)
+            #pragma omp taskloop grainsize(rows/64) reduction(+:visited)
             for (int i = 1; i < rows; i++) {
                 S[i][0] = i * gap_penalty;
                 visited++;
             }
 
-            #pragma omp taskloop grainsize(64) reduction(+:visited)
+            #pragma omp taskloop grainsize(cols/64) reduction(+:visited)
             for (int j = 0; j < cols; j++) {
                 S[0][j] = j * gap_penalty;
                 visited++;
             }
         }
     }
+
+    51480*53640/32
 
     // Iterate over all diagonals
 	for (unsigned int i = 1; i < rows; i++) {
