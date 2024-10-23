06cd844d49a16b9b2f8803dd92fdc2065e2301b3
Ladanov_Nick <nv.ladanov@gmail.com>
2024-10-23 11:47:38 +0200

    feat: first attempt

--- implementation.hpp
+++ implementation.hpp
@@ -40,34 +40,57 @@ unsigned long SequenceInfo::gpsa_taskloop(float** S, int block_size = 1) {
     {
         #pragma omp single
         {
-            // Create tasks for both the first column and first row in parallel
             #pragma omp taskloop grainsize(rows/64) reduction(+:visited)
             for (int i = 1; i < rows; i++) {
-                S[i][0] = i * gap_penalty;
+                S[0][i] = i * gap_penalty;
                 visited++;
             }
 
             #pragma omp taskloop grainsize(cols/64) reduction(+:visited)
             for (int j = 0; j < cols; j++) {
-                S[0][j] = j * gap_penalty;
+                S[j][0] = j * gap_penalty;
                 visited++;
             }
         }
     }
 
 
-    // Iterate over all diagonals
-	for (unsigned int i = 1; i < rows; i++) {
-		for (unsigned int j = 1; j < cols; j++) {
-			float match = S[i - 1][j - 1] + (X[i - 1] == Y[j - 1] ? match_score : mismatch_score);
-			float del = S[i - 1][j] + gap_penalty;
-			float insert = S[i][j - 1] + gap_penalty;
-			S[i][j] = std::max({match, del, insert});
-		
-			visited++;
-		}
-	}
+    int num_blocks_x = (rows + block_size - 1) / block_size;
+    int num_blocks_y = (cols + block_size - 1) / block_size;
     
+    for (int block_row = 0; block_row < num_blocks_x; block_row++) {
+        #pragma omp parallel
+        {
+            #pragma omp single
+            {
+                #pragma omp taskloop reduction(+:visited)
+                for (int block_col = 0; block_col <  num_blocks_y; block_col++) {
+                        // Параллельная обработка блоков в рамках одной горизонтали
+                        // Определяем границы блока
+                        int start_row = block_row * block_size + 1;
+                        int end_row = std::min((block_row + 1) * block_size, rows);
+                        int start_col = block_col * block_size + 1;
+                        int end_col = std::min((block_col + 1) * block_size, cols);
+
+                        // Вычисления внутри каждого блока по диагональной схеме
+                        #pragma omp taskloop reduction(+:visited)
+                        for (int diag = 0; diag < (end_row - start_row + 1) + (end_col - start_col + 1) - 1; diag++) {
+                            for (int i = std::min(start_row + diag, end_row); i > start_row && (i - start_row) < (end_col - start_col); i--) {
+                                int j = start_col + diag - (i - start_row);
+                                if (j < end_col) {
+                                    float match = S[i - 1][j - 1] + (X[i - 1] == Y[j - 1] ? match_score : mismatch_score);
+                                    float del = S[i - 1][j] + gap_penalty;
+                                    float insert = S[i][j - 1] + gap_penalty;
+                                    S[i][j] = std::max({match, del, insert});
+                                    visited++;  // Счётчик посещённых ячеек
+                                }
+                            }
+                        }
+                }
+            }
+        }
+    }
+
     return visited;
 }
 
