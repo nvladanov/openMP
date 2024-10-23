fb1a8aa66b51b4c626ac9760d3dfea47ec18e543
Ladanov_Nick <nv.ladanov@gmail.com>
2024-10-23 09:57:45 +0200

    feat: first attempt

--- implementation.hpp
+++ implementation.hpp
@@ -17,16 +17,16 @@ unsigned long SequenceInfo::gpsa_sequential(float** S) {
 	}
 	
 	// Main part
-	// for (unsigned int i = 1; i < rows; i++) {
-	// 	for (unsigned int j = 1; j < cols; j++) {
-	// 		float match = S[i - 1][j - 1] + (X[i - 1] == Y[j - 1] ? match_score : mismatch_score);
-	// 		float del = S[i - 1][j] + gap_penalty;
-	// 		float insert = S[i][j - 1] + gap_penalty;
-	// 		S[i][j] = std::max({match, del, insert});
+	for (unsigned int i = 1; i < rows; i++) {
+		for (unsigned int j = 1; j < cols; j++) {
+			float match = S[i - 1][j - 1] + (X[i - 1] == Y[j - 1] ? match_score : mismatch_score);
+			float del = S[i - 1][j] + gap_penalty;
+			float insert = S[i][j - 1] + gap_penalty;
+			S[i][j] = std::max({match, del, insert});
 		
-	// 		visited++;
-	// 	}
-	// }
+			visited++;
+		}
+	}
 
     return visited;
 }
@@ -49,30 +49,30 @@ unsigned long SequenceInfo::gpsa_taskloop(float** S, int grain_size = 1) {
     }
 
     // Parallelize the core loop using OpenMP taskloop and reduction
-    // int match_score = 1, mismatch_score = -1;
-    // #pragma omp parallel
-    // {
-    //     #pragma omp single
-    //     {
-    //         for (unsigned int k = 1; k < rows + cols - 1; k++) {
-    //             #pragma omp task depend(inout: S) firstprivate(k) // Create tasks per diagonal
-    //             {
-    //                 unsigned int start_row = (k < rows) ? k : rows - 1;
-    //                 unsigned int start_col = (k < rows) ? 1 : k - rows + 1;
+    int match_score = 1, mismatch_score = -1;
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            for (unsigned int k = 1; k < rows + cols - 1; k++) {
+                #pragma omp task depend(inout: S) firstprivate(k) // Create tasks per diagonal
+                {
+                    unsigned int start_row = (k < rows) ? k : rows - 1;
+                    unsigned int start_col = (k < rows) ? 1 : k - rows + 1;
 
-    //                 for (unsigned int i = start_row, j = start_col; i >= 1 && j < cols; i--, j++) {
-    //                     float match = S[i - 1][j - 1] + (X[i - 1] == Y[j - 1] ? match_score : mismatch_score);
-    //                     float del = S[i - 1][j] + gap_penalty;
-    //                     float insert = S[i][j - 1] + gap_penalty;
-    //                     S[i][j] = std::max({match, del, insert});
+                    for (unsigned int i = start_row, j = start_col; i >= 1 && j < cols; i--, j++) {
+                        float match = S[i - 1][j - 1] + (X[i - 1] == Y[j - 1] ? match_score : mismatch_score);
+                        float del = S[i - 1][j] + gap_penalty;
+                        float insert = S[i][j - 1] + gap_penalty;
+                        S[i][j] = std::max({match, del, insert});
                         
-    //                     #pragma omp atomic
-    //                     visited++;
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
+                        #pragma omp atomic
+                        visited++;
+                    }
+                }
+            }
+        }
+    }
 
     return visited;
 }
